b8c3711afe32d72ce927beca17577d5d27f98efe
aishahassanshah <54057328+aishahassanshah@users.noreply.github.com>
2021-02-21 20:41:48 +1100

    Add files via upload

---
+++ C#/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace primeNumbers
+{
+     class primeNumbers
+    {
+        public static void Main()
+        {
+            int start_number, end_number;
+            Console.Write("Input starting number of range: ");
+            start_number = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Input ending number of range : ");
+            end_number = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("The prime numbers between {0} and {1} are : \n", start_number, end_number);
+
+
+            for (int number= start_number; number<= end_number; number++)
+            {
+                int count = 0;
+                for(int i=1;i<=number;i++)
+                {
+                    if (number%i==0)
+                    {
+                        count++;
+                    }
+                }
+                if(count==2)
+                {
+                    Console.WriteLine(number);
+                }
+            }
+            Console.ReadLine();
+            
+        }
+    }
+}
