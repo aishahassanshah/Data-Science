10ccd43e6dd206a59e1a9aceb643fb6f9962ac23
aishahassanshah <54057328+aishahassanshah@users.noreply.github.com>
2021-05-04 20:39:39 +1000

    Add files via upload

---
+++ C#/Student Bot/StudentDetail.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StudentBot
+{
+    public class StudentDetail
+    {
+        public string BotName { get; set; }
+        public string StudentName { get; set; }
+        public string SchoolName { get; set; }
+        public string BoardName { get; set; }
+        public string SchoolYear { get; set; }
+        public string SchoolClass { get; set; }
+        public string SchoolClassSection { get; set; }
+        public string UserRollNumber { get; set; }
+
+        public string EmailAdderss { get; set; }
+        public string SocialMediaProfile { get; set; }
+        public string SocialMediaProfileLink { get; set; }
+        public string Personality { get; set; }
+        public string AcademicSubjects { get; set; }
+        public string Habits { get; set; }
+        public string AspirationProfession { get; set; }
+    }
+}
+
+
